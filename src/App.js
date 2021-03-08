01b26fe21712f3249ee498f6c724aacc712edb15
nader chargui <naderchargui@gmail.com>
2021-03-09 12:13:55 +1245

    this is work  by  component function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import imageone from './images/photo1.jfif';
+import  Navbar  from './component/Navbar/Navbar.js';
+import  Header from    './component/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Navbar/>
+       <Header/>
+      <form className="blockform">
+
+      <div className="formimg">
+      <img  src={imageone}/>
+ 
+  </div>
+  <div className="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+ 
+  </div>
+  <div className="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="form-group form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
       </header>
     </div>
   );
---
+++ src/component/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import './Header.css';
+function Header() {
+    return (
+       <div className="container">
+            <div   className="blockheader">
+            <div className="blockone">
+               
+               <h2>
+                   welcome in <span> react </span>
+               </h2>
+            </div>
+             <div className="blocktwo">
+                 <p>
+
+                 loremIt was disturbing to think of the Flatline as a gliding cursor struck sparks from the missionaries, the train reached Case’s station. Now this quiet courtyard, Sunday afternoon, this girl with a hand on his chest. Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous reconstruction’s of garments of the car’s floor. Case had never seen him wear the same suit twice, although his wardrobe seemed to consist entirely of meticulous 
+                 </p>
+
+             </div>
+        </div>
+       </div>
+    )
+}
+
+export default Header
+
+
+//rfce 
